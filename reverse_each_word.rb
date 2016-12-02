c4f6bd65144c34501c7fd07235b1e1f24d790298
Cedric David <cedric.david@btopenworld.com>
2016-12-02 03:06:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+
+  array = string.split(" ")
+  new_array = []
+  array.collect do |word|
+    new_array.push(word.reverse!)
+
+  end
+  reversed_string = new_array.join(" ")
+  return  reversed_string
+
+end
