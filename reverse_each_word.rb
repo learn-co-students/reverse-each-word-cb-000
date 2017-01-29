52499ab9a9f7dce885d19d39879caf65831f156e
Salvatore McCarty <salvatore.mccarty@gmail.com>
2017-01-29 22:30:33 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+=begin
+def reverse_each_word(sentence)
+    word_array = split_string(sentence)
+    word_array = reverse_array(word_array)
+    output = word_array.join(" ") 
+end
+
+def split_string(string)
+  return string.split
+end
+
+def reverse_array(array)
+  reversed_array = []
+  array.each {|e| reversed_array << e.reverse}
+  return reversed_array
+end
+=end
+def reverse_each_word(sentence)
+    word_array = sentence.split
+    output = (word_array.collect { |e| e.reverse }).join(" ")
+end
+
