d65e4e1b6b02e9c6eb5b66ad3e5abd2da6a29850
James Wilhelm <hello@jamesaverywilhelm.com>
2017-02-27 22:54:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   reversed_words = []
   sentence.split(" ").collect do |word|
-    reversed_words << word.reverse
+    word.reverse
   end
-  reversed_words.join(" ")
 end
