ad4aec3073dbf2237d53d92bf74f8700b80916d7
Cleavon Brathwaite <cbrathwaite90@gmail.com>
2017-04-06 19:51:26 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
-sentence = ["my name is cleavon"]
+
 def reverse_each_word(sentence)
-  sentence.each do |word|
-    puts.word.reverse
+  words = sentence.split
+  words.collect do |word|
+    word.reverse!
   end
+  words.join(" ")
 end
+
