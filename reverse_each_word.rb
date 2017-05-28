053121c7f87d4e88f18f760a58d45401d32dcde7
Tony Nassif <tonynassif@gmail.com>
2017-05-28 00:22:19 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,6 @@ end
 =end
 
 def reverse_each_word(sentence)
-  word_array = word_array.collect { |word| word.reverse }
-  word_array.split(" ")
+  word_array = sentence.split
+  word_array.collect { |word| word.reverse }.join(" ")
 end
