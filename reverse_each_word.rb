23a07561e5e3e555c6885374eaa7573498bdabf6
jmoypace <jmoypace@gmail.com>
2018-06-26 02:49:38 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,15 @@
 #  sentence.split.collect {|word| word.reverse}.join(" ")
 #end
 
+#def reverse_each_word(sentence)
+#newarr=sentence.split(" ")
+#newarr1=[]
+#newarr.collect do |word|
+#newarr1.push(word.reverse)
+#end
+# newarr1.join(" ")
+#end
 def reverse_each_word(sentence)
-newarr=sentence.split(" ")
-newarr1=[]
-newarr.collect do |word|
-newarr1.push(word.reverse)
-end
- newarr1.join(" ")
+  newarr=sentence.split(" ")
+  newarr.collect{|word| word.reverse}.join(" ")
 end
